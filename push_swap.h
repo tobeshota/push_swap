23648a9ae5ae37c41b9287e6a5edc92855b4c6b6
toshota <toshota@student.42tokyo.jp>
2023-10-09 16:28:40 +0900

    write an implementation policy

---
+++ push_swap.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: toshota <toshota@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/09 16:23:25 by toshota           #+#    #+#             */
+/*   Updated: 2023/10/09 16:27:01 by toshota          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+#include "libft/libft.h"
+#include "operations/operations.h"
+#include "sort/sort.h"
+#include "utils/utils.h"
+
+#endif
