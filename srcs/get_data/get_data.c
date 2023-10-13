714ce308200b74962ca677fc4e279c5eb8638649
戸部翔太 <tobeshouta@Intel.local>
2023-10-13 17:46:26 +0900

    begint to implement get_data();

--- srcs/get_data/get_data.c
+++ srcs/get_data/get_data.c
@@ -6,7 +6,7 @@
 /*   By: toshota <toshota@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 17:24:49 by toshota           #+#    #+#             */
-/*   Updated: 2023/10/13 15:38:32 by toshota          ###   ########.fr       */
+/*   Updated: 2023/10/13 17:46:17 by toshota          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,5 @@
 void	get_data(int argc, char **argv, t_data *data)
 {
 	// 個々のコマンドライン引数に格納されている値（数値・）をリストに代入する
-	// get_data_from_argv(argv, data);
+	// get_data_from_argv(argv + 1, data);
 }
